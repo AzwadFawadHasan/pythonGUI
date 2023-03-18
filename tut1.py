e2ff6d0f68d0ddb4ede59c4cac3a64fa866a69a5
AzwadFawadHasan <azwadfawadhasan@gmail.com>
2023-03-18 16:22:05 +0600

    added myRoot.max and min size

---
+++ tut1.py
@@ -0,0 +1,24 @@
+print("hellow")
+#import tkinter
+from tkinter import *
+
+#azwad_root = Tk()
+#
+##gui logic
+#
+#azwad_root.mainloop()
+##Question = other than Tkinker what are the ways we can create a gui in python?
+
+myRoot = Tk()
+
+# Width x Height
+myRoot.geometry("644x234")
+
+#width, height
+myRoot.minsize(200,200)
+
+#width, height
+myRoot.maxsize(1200,899)
+
+
+myRoot.mainloop()
